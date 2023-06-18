31161a7c3b10f420e0ce576e988eed0ea2d47ef0
DeveKoke <jorge.be86@gmail.com>
2023-06-18 17:45:22 +0200

    last_styles_home

--- public/scripts.js
+++ public/scripts.js
@@ -1,31 +1,30 @@
 //* HAMBURGUER MENU DEL NAVBAR Y SU DESPLIEGUE   ---------------------------------
-// const menu = document.getElementById('menu')
-// const menuIcon = document.getElementById('menu_icon')
-// const menuUl = document.createElement('ul'); 
-// menuUl.setAttribute('class','menu_ul');
-// if(menuIcon){
-//   menuIcon.addEventListener('click', () => {
-//     const menuItems = `
-//     <li><a href="drivers.html">DRIVERS</a></li>
-//     <li><a href="home.html">YOUR TEAM</a></li>
-//     <li><a href="resultados.html">RESULTS</a></li>
-//     <li><a href="liga.html">LEAGUE</a></li>
-//   `;
-//   if (menuUl.style.display === 'none') {
-//     setTimeout(function() {
-//       menu.style.opacity = '1';
-//     }, 100);
-//     menuUl.style.display = 'flex';
-//     menu.style.borderTop = 'solid';
-//     menu.style.borderTopColor = 'rgb(203, 25, 25)';
-//   } else {
-//     menuUl.style.display = 'none';
-//     menu.style.borderTop = 'none';
-//   }
-//   menu.appendChild(menuUl);
-//   menuUl.innerHTML= menuItems;
-//   });
-// }
+const menu = document.getElementById('menu') // div del menú desplegado
+const menuIcon = document.getElementById('menu_icon')  //icono hamburguer
+const menuUl = document.createElement('ul'); 
+menuUl.setAttribute('class','menu_ul');
+if(menuIcon){
+  menuIcon.addEventListener('click', () => {
+    const menuItems = `
+    <li><a href="drivers.html">FAVORITES</a></li>
+    <li><a href="home.html">USERS</a></li>
+    <li><a href="resultados.html">DASHBOARD</a></li>
+  `;
+  if (menuUl.style.display === 'none') {
+    setTimeout(function() {
+      menu.style.opacity = '1';
+    }, 100);
+    menuUl.style.display = 'flex';
+    menu.style.borderTop = 'solid';
+    menu.style.borderTopColor = 'rgb(203, 25, 25)';
+  } else {
+    menuUl.style.display = 'none';
+    menu.style.borderTop = 'none';
+  }
+  menu.appendChild(menuUl);
+  menuUl.innerHTML= menuItems;
+  });
+}
 
 function validateSearch(event) {
         if(/^([a-zA-Z\s-]+)/gm.test(event.target.position.value) == true && /^([a-zA-Z\s-]+)/gm.test(event.target.location.value) == true) {
