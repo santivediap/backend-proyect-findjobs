fd2e1bc39dce38a35bcf54d0d06ab0f51525884a
DeveKoke <jorge.be86@gmail.com>
2023-06-23 09:07:22 +0200

    offers searched view created

--- controllers/viewControllers.js
+++ controllers/viewControllers.js
@@ -7,11 +7,14 @@ const userProfile = (req, res) => {
 const UserFavorites = (req, res) => {
     res.status(200).render("userFavorites.pug")
 }
-
+const searchResult = (req, res) => {
+    res.status(200).render("search_results.pug")
+}
 
 module.exports = {
     homeSearch,
     userProfile,
-    UserFavorites
+    UserFavorites,
+    searchResult
 
 }
--- routes/viewsRoutes.js
+++ routes/viewsRoutes.js
@@ -5,6 +5,7 @@ const viewController = require ('../controllers/viewControllers');
 actionRouter.get('/', viewController.homeSearch);
 actionRouter.get('/users/profile', viewController.userProfile);
 actionRouter.get('/favorites', viewController.UserFavorites);
+actionRouter.get('/search-result', viewController.searchResult);
 
 
 module.exports = actionRouter;
