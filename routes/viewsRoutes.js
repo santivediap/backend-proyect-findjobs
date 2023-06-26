da89f5fcf423523b63126ef82752a10ef3a90b92
SVega9848 <santivediap@gmail.com>
2023-06-26 16:02:45 +0200

    Add new post search route

--- routes/viewsRoutes.js
+++ routes/viewsRoutes.js
@@ -6,6 +6,7 @@ actionRouter.get('/', viewController.homeSearch);
 actionRouter.get('/users/profile', viewController.userProfile);
 actionRouter.get('/user/favorites', viewController.userFavorites);
 actionRouter.get('/search-results', viewController.searchResult);
+actionRouter.post('/search-results', viewController.searchOffers);
 actionRouter.get('/user-login', viewController.userLogin);
 actionRouter.get('/create-account', viewController.userSignUp);
 
