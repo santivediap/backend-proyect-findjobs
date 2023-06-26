2c3bc6718006ec95a52e0d18a78716c2af3c3fb5
SVega9848 <santivediap@gmail.com>
2023-06-26 17:45:27 +0200

    Add role field to createUser query

--- queries/users.queries.js
+++ queries/users.queries.js
@@ -6,8 +6,8 @@ const usersQueries = {
         FROM users
         where email=$1;`,
     createUser: `INSERT INTO users(
-        name, surname, email, city, password, logged)
-        VALUES ($1, $2, $3, $4, $5, FALSE);`,
+        name, surname, email, city, password, role, logged)
+        VALUES ($1, $2, $3, $4, $5, 'user', FALSE);`,
     updateUser: `UPDATE users
         SET logged=$1
         WHERE email =$2;`
