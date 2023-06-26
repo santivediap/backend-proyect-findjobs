db5d461a30bea291a9ecb7657a58a1ba2e4ddaf4
SVega9848 <santivediap@gmail.com>
2023-06-26 20:18:24 +0200

    Disable almost all views routes

--- routes/viewsRoutes.js
+++ routes/viewsRoutes.js
@@ -4,13 +4,13 @@ const viewController = require ('../controllers/viewControllers');
 const protectedRoutes = require ('../middlewares/verifiedToken');
 
 actionRouter.get('/', protectedRoutes, viewController.homeSearch);
-actionRouter.get('/users/profile', protectedRoutes, viewController.userProfile);
-actionRouter.get('/user/favorites', viewController.userFavorites);
-actionRouter.get('/search-results', viewController.searchResult);
-actionRouter.post('/search-results', viewController.searchOffers);
-actionRouter.get('/user-login', viewController.userLogin);
-actionRouter.get('/create-account', viewController.userSignUp);
-actionRouter.get('/home', viewController.adminHome);
+// actionRouter.get('/users/profile', protectedRoutes, viewController.userProfile);
+// actionRouter.get('/user/favorites', viewController.userFavorites);
+// actionRouter.get('/search-results', viewController.searchResult);
+// actionRouter.post('/search-results', viewController.searchOffers);
+// actionRouter.get('/user-login', viewController.userLogin);
+// actionRouter.get('/create-account', viewController.userSignUp);
+// actionRouter.get('/home', viewController.adminHome);
 
 
 module.exports = actionRouter;
