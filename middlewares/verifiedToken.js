5c7e84cb3aaac59357e745cf302468066e600131
SVega9848 <santivediap@gmail.com>
2023-06-26 17:46:20 +0200

    Add errors management

--- middlewares/verifiedToken.js
+++ middlewares/verifiedToken.js
@@ -9,26 +9,31 @@ protectedRoutes.use((req, res, next) => {
     // const token = req.headers.cookie;
     // const cookie = token.split("=")
     const token = req.cookies['access-token']  
+
     if (token) {
       jwt.verify(token, jwt_secret, async (err, decoded) => {
-        console.log(decoded);
-        let data = await User.getUserByEmail(decoded.email)
 
-        console.log(data);
-
-        if (data[0].logged === true) {
-          req.decoded = decoded;
-          
-          console.log(`LOG: ${req.decoded}`);
-          next();   
+        if(decoded == undefined) {
+          req.decoded == undefined
+          next()
         } else {
-          return res.json({ msg: 'Invalid token' });
+          console.log(decoded);
+          let data = await User.getUserByEmail(decoded.email)
+  
+          console.log(data);
+  
+          if (data[0].logged === true) {
+            req.decoded = decoded;
+
+            next();   
+          } else {
+            return res.json({ msg: 'Invalid token' });
+          }
         }
       });
     } else {
-      res.send({ 
-          msg: 'Token not provided' 
-      });
+      req.decoded = null;
+      next()
     }
  });
 
