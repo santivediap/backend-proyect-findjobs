0355d69e1fc2db97873323f28b812e3f240986c2
Amir <Amirhosseinshirzad@users.noreply.github.com>
2023-06-27 18:35:17 +0200

    Fix player not found problem

--- middlewares/verifiedToken.js
+++ middlewares/verifiedToken.js
@@ -1,35 +1,34 @@
-const User = require('../models/usersModels');
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const User = require("../models/usersModels");
+const express = require("express");
+const jwt = require("jsonwebtoken");
 const jwt_secret = "tortilla";
 
 const protectedRoutes = express.Router();
 
 protectedRoutes.use((req, res, next) => {
-    // const token = req.headers.cookie;
-    // const cookie = token.split("=")
-    const token = req.cookies['access-token']  
-    if (token) {
-      jwt.verify(token, jwt_secret, async (err, decoded) => {
-        console.log(decoded);
-        let data = await User.getUserByEmail(decoded.email)
+  // const token = req.headers.cookie;
+  // const cookie = token.split("=")
+  const token = req.cookies["access-token"];
+  if (token) {
+    jwt.verify(token, jwt_secret, async (err, decoded) => {
+      console.log(decoded);
+      let data = await User.getUserByEmail(decoded.email);
 
-        console.log(data);
+      console.log(data);
 
-        if (data[0].logged === true) {
-          req.decoded = decoded;
-          
-          console.log(`LOG: ${req.decoded}`);
-          next();   
-        } else {
-          return res.json({ msg: 'Invalid token' });
-        }
-      });
-    } else {
-      res.send({ 
-          msg: 'Token not provided' 
-      });
-    }
- });
+      if (data[0].logged === true) {
+        req.decoded = decoded;
+
+        console.log(`LOG: ${req.decoded}`);
+        next();
+      } else {
+        return res.json({ msg: "Invalid token" });
+      }
+    });
+  } else {
+    req.decoded = null;
+    next();
+  }
+});
 
 module.exports = protectedRoutes;
