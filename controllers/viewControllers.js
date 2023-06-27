296fb287520f8ff43a572ee414e46e82905f8f31
Amir <Amirhosseinshirzad@users.noreply.github.com>
2023-06-27 18:36:48 +0200

    Add favorites funcionalities

--- controllers/viewControllers.js
+++ controllers/viewControllers.js
@@ -1,90 +1,126 @@
-const scraperInsertia = require('../utils/scraperInsertia')
-const scraperJobatus = require('../utils/scraperJobatus')
+const scraperInsertia = require("../utils/scraperInsertia");
+const scraperJobatus = require("../utils/scraperJobatus");
 const Offer = require("../models/offers");
+const users = require("../models/usersModels");
+const favs = require("../models/favoritesModels");
 
-const homeSearch =  (req, res) => {
-    res.status(200).render("home_out.pug")
-}
+const homeSearch = (req, res) => {
+  res.status(200).render("home_out.pug");
+};
 const userProfile = (req, res) => {
-    const { name, surname, email, city } = req.decoded;
-    console.log(req.decoded);
-    res.status(200).render("profile.pug", {
-        name: name,
-        surname:surname,
-        email: email,
-        city: city
-    });
-}
-const userFavorites = (req, res) => {
-    res.status(200).render("userFavorites.pug")
-}
+  const { name, surname, email, city } = req.decoded;
+  console.log(req.decoded);
+  res.status(200).render("profile.pug", {
+    name: name,
+    surname: surname,
+    email: email,
+    city: city,
+  });
+};
+const userFavorites = async (req, res) => {
+  console.log("FAVORITOS!");
+
+  console.log(req.decoded.email);
+  // console.log(searchedUser[0].user_id);
+  const searchedUser = await users.getUserByEmail(req.decoded.email);
+  console.log(searchedUser[0].email);
+  const favoritesList = await favs.getAllFavorites(searchedUser[0].email);
+
+  console.log(favoritesList);
+
+  res.status(200).render("userFavorites", {
+    scrap: favoritesList,
+  });
+};
 
 const searchResult = async (req, res) => {
+  console.log("USER BUSCA");
 
-    let locationRegex = new RegExp(`${req.query.location}`)
-    let titleRegex = new RegExp(`${req.query.position}`)
+  let locationRegex = new RegExp(`${req.query.location}`);
+  let titleRegex = new RegExp(`${req.query.position}`);
 
-    let insertiaResults = await scraperInsertia.scrapOfferData(`https://www.insertia.net/trabajo-de-${req.query.position.toLowerCase()}/${req.query.location.toLowerCase()}-${req.query.location.toLowerCase()}`)
-                .then(insertia => insertia)
+  let insertiaResults = await scraperInsertia
+    .scrapOfferData(
+      `https://www.insertia.net/trabajo-de-${req.query.position.toLowerCase()}/${req.query.location.toLowerCase()}-${req.query.location.toLowerCase()}`
+    )
+    .then((insertia) => insertia);
 
-    let jobatusResults = await scraperJobatus.scrapOfferData(`https://www.jobatus.es/trabajo?q=${req.query.position}&l=${req.query.location}&jb=all&sort=&d=&page=1`)
-                .then(jobatus => jobatus)
+  let jobatusResults = await scraperJobatus
+    .scrapOfferData(
+      `https://www.jobatus.es/trabajo?q=${req.query.position}&l=${req.query.location}&jb=all&sort=&d=&page=1`
+    )
+    .then((jobatus) => jobatus);
 
-    let databaseResults = await Offer.find({ "title" : { $regex: titleRegex, $options: 'i' }, "location" : { $regex: locationRegex, $options: 'i' } }, "-_id -__v");
-                
-    const result = [];
+  let databaseResults = await Offer.find(
+    {
+      title: { $regex: titleRegex, $options: "i" },
+      location: { $regex: locationRegex, $options: "i" },
+    },
+    "-_id -__v"
+  );
 
-    if(insertiaResults != null) {
-        result.push(...insertiaResults)
-    }
+  console.log(databaseResults);
 
-    if(jobatusResults != null) {
-        result.push(...jobatusResults)
-    }
+  const result = [];
 
-    if(databaseResults.length > 0) {
-        result.push(...databaseResults)
-    }
+  if (insertiaResults != null) {
+    result.push(...insertiaResults);
+  }
 
-    if(result.length < 1) {
-        res.status(200).render("search_results", {
-            "scrap": [{"error": "NOT FOUND"}],
-            "position": req.query.position,
-            "location": req.query.location
-        })
+  if (jobatusResults != null) {
+    result.push(...jobatusResults);
+  }
+
+  if (databaseResults.length > 0) {
+    result.push(...databaseResults);
+  }
+
+  if (result.length < 1) {
+    res.status(200).render("search_results", {
+      scrap: [{ error: "NOT FOUND" }],
+      position: req.query.position,
+      location: req.query.location,
+    });
+  } else {
+    if (req.decoded == null) {
+      res.status(200).render("search_results", {
+        scrap: result,
+        position: req.query.position,
+        location: req.query.location,
+      });
     } else {
-        res.status(200).render("search_results", {
-            "scrap": result,
-            "position": req.query.position,
-            "location": req.query.location
-        })
+      res.status(200).render("search_results_logged", {
+        scrap: result,
+        position: req.query.position,
+        location: req.query.location,
+      });
     }
-}
+  }
+};
 
 const searchOffers = async (req, res) => {
+  try {
+    await res.redirect(
+      `/search-results?position=${req.body.position}&location=${req.body.location}`
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-    try {
-        await res.redirect(`/search-results?position=${req.body.position}&location=${req.body.location}`)
-    } catch (err) {
-        console.log(err);
-    }
-
-}
-
-const userLogin  = (req, res) => {
-    res.status(200).render("loginUser.pug")
-}
-const userSignUp  = (req, res) => {
-    res.status(200).render("createAccount.pug")
-}
+const userLogin = (req, res) => {
+  res.status(200).render("loginUser.pug");
+};
+const userSignUp = (req, res) => {
+  res.status(200).render("createAccount.pug");
+};
 
 module.exports = {
-    homeSearch,
-    userProfile,
-    userFavorites,
-    searchResult,
-    userLogin,
-    userSignUp,
-    searchOffers
-
-}
+  homeSearch,
+  userProfile,
+  userFavorites,
+  searchResult,
+  userLogin,
+  userSignUp,
+  searchOffers,
+};
