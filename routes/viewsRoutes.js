50e24d284670c6d2afae7f35fc2a6f299cacbe4e
Amir <Amirhosseinshirzad@users.noreply.github.com>
2023-06-27 18:24:09 +0200

    Add routes protection

--- routes/viewsRoutes.js
+++ routes/viewsRoutes.js
@@ -1,15 +1,22 @@
-const express = require('express');
-const actionRouter = express.Router(); 
-const viewController = require ('../controllers/viewControllers');
-const protectedRoutes = require ('../middlewares/verifiedToken');
+const express = require("express");
+const actionRouter = express.Router();
+const viewController = require("../controllers/viewControllers");
+const protectedRoutes = require("../middlewares/verifiedToken");
 
-actionRouter.get('/', viewController.homeSearch);
-actionRouter.get('/users/profile', protectedRoutes, viewController.userProfile);
-actionRouter.get('/user/favorites', viewController.userFavorites);
-actionRouter.get('/search-results', viewController.searchResult);
-actionRouter.post('/search-results', viewController.searchOffers);
-actionRouter.get('/user-login', viewController.userLogin);
-actionRouter.get('/create-account', viewController.userSignUp);
-
+actionRouter.get("/", viewController.homeSearch);
+actionRouter.get("/users/profile", protectedRoutes, viewController.userProfile);
+actionRouter.get(
+  "/user/favorites",
+  protectedRoutes,
+  viewController.userFavorites
+);
+actionRouter.get(
+  "/search-results",
+  protectedRoutes,
+  viewController.searchResult
+);
+actionRouter.post("/search-results", viewController.searchOffers);
+actionRouter.get("/user-login", viewController.userLogin);
+actionRouter.get("/create-account", viewController.userSignUp);
 
 module.exports = actionRouter;
