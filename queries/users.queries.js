796a872bcafa15a4e193856adca6c9e761db1513
Amir <Amirhosseinshirzad@users.noreply.github.com>
2023-06-27 18:26:05 +0200

    Add user_id field to getUserByEmail query

--- queries/users.queries.js
+++ queries/users.queries.js
@@ -1,17 +1,15 @@
 const usersQueries = {
-    getAllUsers: `SELECT name, surname, email, city 
+  getAllUsers: `SELECT name, surname, email, city 
         FROM users;`,
-    getUserByEmail: `
-        SELECT name, surname, email, city, password, logged
+  getUserByEmail: `
+        SELECT user_id, name, surname, email, city, password, logged
         FROM users
         where email=$1;`,
-    createUser: `INSERT INTO users(
+  createUser: `INSERT INTO users(
         name, surname, email, city, password, logged)
         VALUES ($1, $2, $3, $4, $5, FALSE);`,
-    updateUser: `UPDATE users
+  updateUser: `UPDATE users
         SET logged=$1
-        WHERE email =$2;`
-}
+        WHERE email =$2;`,
+};
 module.exports = usersQueries;
-
-
